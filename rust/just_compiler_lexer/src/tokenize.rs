63e51ec599745ffe5c0371e5b26dccf5451cb6c7
unional <homawong@gmail.com>
2020-11-30 00:07:41 -0800

    update is_white_space

--- rust/just_compiler_lexer/src/tokenize.rs
+++ rust/just_compiler_lexer/src/tokenize.rs
@@ -49,7 +49,7 @@ impl Cursor<'_> {
   fn advance_token(&mut self) -> Token {
     let first_char = self.bump().unwrap();
     let token_kind = match first_char {
-      ' ' => Whitespace,
+      c if is_whitespace(c) => Whitespace,
       c if is_id_start(c) => self.identifier(),
       _ => Unknown,
     };
@@ -66,6 +66,38 @@ impl Cursor<'_> {
       self.bump();
     }
   }
+}
+
+/// True if `c` is considered a whitespace according to Rust language definition.
+/// See [Rust language reference](https://doc.rust-lang.org/reference/whitespace.html)
+/// for definitions of these classes.
+pub fn is_whitespace(c: char) -> bool {
+  // This is Pattern_White_Space.
+  //
+  // Note that this set is stable (ie, it doesn't change with different
+  // Unicode versions), so it's ok to just hard-code the values.
+
+  matches!(
+    c,
+    // Usual ASCII suspects
+    '\u{0009}'   // \t
+        | '\u{000A}' // \n
+        | '\u{000B}' // vertical tab
+        | '\u{000C}' // form feed
+        | '\u{000D}' // \r
+        | '\u{0020}' // space
+
+        // NEXT LINE from latin1
+        | '\u{0085}'
+
+        // Bidi markers
+        | '\u{200E}' // LEFT-TO-RIGHT MARK
+        | '\u{200F}' // RIGHT-TO-LEFT MARK
+
+        // Dedicated whitespace characters from Unicode
+        | '\u{2028}' // LINE SEPARATOR
+        | '\u{2029}' // PARAGRAPH SEPARATOR
+  )
 }
 
 /// True if `c` is valid as a first character of an identifier.
